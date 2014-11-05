e643e283de22b98328a62c7277476ab248dfc0ff
utnso <utnso@utn.so>
2014-11-05 00:25:08 -0300

    Tabla fea

--- bicis.js
+++ bicis.js
@@ -69,7 +69,21 @@ $.ajax({
     url: 'https://api.morph.io/utnso/funciona/data.json?key=VqRWxvYWXds%2BZDw3OHHn&query=select%20*%20from%20%27data%27',
     dataType: 'jsonp',
     success: function(data) {
-      $('#bicis-tabla').html(JSON.stringify(data, undefined, 2))
+      var theTable = document.createElement('table');
+      document.getElementById('bicis-tabla').appendChild(theTable);
+      for(index in data) {
+        var estacion = data[index];
+        var theRow = document.createElement('tr');
+        theTable.appendChild(theRow);
+        var theTd = document.createElement('td');
+        theTd.textContent = estacion['name'];
+        theRow.appendChild(theTd);
+        theTd = document.createElement('td');
+        theTd.textContent = estacion['available'];
+        theRow.appendChild(theTd);
+
+      }
+      // $('#bicis-tabla').html(JSON.stringify(data, undefined, 2))
         // console.log('received data: ' + );
     }
 });
